9227b98fa4a76ab0f6929cebbbb14779e543b947
Corey Farwell <coreyf@rwell.org>
2016-04-02 22:00:49 -0400

    more cleanup, more error handling

--- src/cargo-fuzz.rs
+++ src/cargo-fuzz.rs
@@ -1,12 +1,13 @@
 use std::env;
+use std::io::{self, Write};
 use std::process::Command;
 
 
 fn main() {
     env::set_var("AFL_DEFER_FORKSRV", "1");
     let args = env::args().skip(1).collect::<Vec<_>>();
-    let output = Command::new("afl-fuzz")
-                          .args(&args)
-                          .status()
-                          .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+    let status = Command::new("afl-fuzz").args(&args).status();
+    if let Err(e) = status {
+        writeln!(io::stderr(), "Could not run 'afl-fuzz': {}", e).unwrap();
+    }
 }
