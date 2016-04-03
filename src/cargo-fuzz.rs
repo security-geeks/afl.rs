ba724d31f607205858535a1119ffb0137204b9fe
Corey Farwell <coreyf@rwell.org>
2016-04-02 21:51:39 -0400

    cleanup imports

--- src/cargo-fuzz.rs
+++ src/cargo-fuzz.rs
@@ -1,15 +1,15 @@
 use std::env;
-use std::process;
+use std::process::{Command, Stdio};
 
 
 fn main() {
     env::set_var("AFL_DEFER_FORKSRV", "1");
     let args = env::args().skip(1).collect::<Vec<_>>();
-    let output = process::Command::new("afl-fuzz")
+    let output = Command::new("afl-fuzz")
                           .args(&args)
-                          .stdin(process::Stdio::inherit())
-                          .stderr(process::Stdio::inherit())
-                          .stdout(process::Stdio::inherit())
+                          .stdin(Stdio::inherit())
+                          .stderr(Stdio::inherit())
+                          .stdout(Stdio::inherit())
                           .output()
                           .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
 }
