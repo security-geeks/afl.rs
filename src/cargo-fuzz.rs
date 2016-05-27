a6449f26004eeab31854275805a601831b34d61f
Corey Farwell <coreyf@rwell.org>
2016-05-26 23:42:01 -0400

    tmp

--- src/cargo-fuzz.rs
+++ src/cargo-fuzz.rs
@@ -2,12 +2,10 @@ use std::env;
 use std::io::{self, Write};
 use std::process::Command;
 
+extern crate afl_sys;
+
 
 fn main() {
     env::set_var("AFL_DEFER_FORKSRV", "1");
-    let args = env::args().skip(1).collect::<Vec<_>>();
-    let status = Command::new("afl-fuzz").args(&args).status();
-    if let Err(e) = status {
-        writeln!(io::stderr(), "Could not run 'afl-fuzz': {}", e).unwrap();
-    }
+    let _ = afl_sys::fuzz::afl_fuzz_env();
 }
